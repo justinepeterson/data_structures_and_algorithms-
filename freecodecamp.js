369758513eaf164fbb13862c4ffdf9fe84a40250
justinepeterson <jmahinyila@gmail.com>
2021-06-26 23:37:02 +0300

    Mutations
    Return true if the string in the first element of the array contains all of the letters of the string in the second element of the array.
    
    For example, [hello, Hello], should return true because all of the letters in the second string are present in the first, ignoring case.
    
    The arguments [hello, hey] should return false because the string hello does not contain a y.
    
    Lastly, [Alien, line], should return true because all of the letters in line are present in Alien.

--- freecodecamp.js
+++ freecodecamp.js
@@ -60,3 +60,14 @@ function getIndexToIns(arr, num) {
   
   console.log(getIndexToIns([2, 5, 10], 15));
   */
+
+
+//Mutations
+function mutation(arr) {
+    let test = arr[1].toLowerCase();
+    let target = arr[0].toLowerCase();
+    for (let i = 0; i < test.length; i++) {
+      if (target.indexOf(test[i]) < 0) return false;
+    }
+    return true;
+  }
