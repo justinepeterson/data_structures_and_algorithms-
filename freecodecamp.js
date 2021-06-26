4522cb8d31df5df8f06106e31a8446cfd87d113e
justinepeterson <jmahinyila@gmail.com>
2021-06-26 22:27:54 +0300

    Slice and Splice
    You are given two arrays and an index.
    
    Copy each element of the first array into the second array, in order.
    
    Begin inserting elements at index n of the second array.
    
    Return the resulting array. The input arrays should remain the same after the function runs.

--- freecodecamp.js
+++ freecodecamp.js
@@ -1,3 +1,4 @@
+/*
 // Title Case a Sentence
 function titleCase(str) {
     let capitlized = []
@@ -8,3 +9,13 @@ function titleCase(str) {
 }
 
 console.log(titleCase("HERE IS MY HANDLE HERE IS MY SPOUT"))
+*/
+
+
+//Slice and Splice
+function frankenSplice(arr1, arr2, n) {
+    let arrComb = [...arr2]
+    arrComb.splice(n, 0, ...arr1)
+    return arrComb;
+  }
+ console.log(frankenSplice([1, 2, 3], [4, 5, 6], 1))
