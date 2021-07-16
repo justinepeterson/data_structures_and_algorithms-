b3bc2bceaf093cd47369f24e3c61aac4a8b8e27c
justinepeterson <jmahinyila@gmail.com>
2021-07-16 21:31:59 +0300

    Translate to pig Latin

--- freecodecamp.js
+++ freecodecamp.js
@@ -178,12 +178,26 @@ function whatIsInAName(collection, source) {
 console.log(whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" }))
 */
 
-function spinalCase(str) {
-  return str.match(/[A-Z]?[a-z]+/g).join('-').toLowerCase()
+function translatePigLatin(str) {
+   // Create variables to be used
+   var pigLatin = "";
+   var regex = /[aeiou]/gi;
+
+   // Check if the first character is a vowel
+  if (str[0].match(regex)) {
+    pigLatin = str + "way";
+  } else if (str.match(regex) === null) {
+    // Check if the string contains only consonants
+    pigLatin = str + "ay";
+  } else {
+    // Find how many consonants before the first vowel.
+    var vowelIndice = str.indexOf(str.match(regex)[0]);
+    // Take the string from the first vowel to the last char
+    // then add the consonants that were previously omitted and add the ending.
+    pigLatin = str.substr(vowelIndice) + str.substr(0, vowelIndice) + "ay";
+  }
+
+  return pigLatin;
 }
 
-console.log(spinalCase('This Is Spinal Tap'));
-console.log(spinalCase("thisIsSpinalTap"))
-console.log(spinalCase("The_Andy_Griffith_Show"))
-console.log(spinalCase("Teletubbies say Eh-oh"))
-console.log(spinalCase("AllThe-small Things"))
+console.log(translatePigLatin("glove"))
